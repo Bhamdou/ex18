6e7b2dd73540062b1fb7dd6fdeabef574dff2883
Bahaa <qgunbb21@gmail.com>
2022-09-15 14:03:36 +0200

    first commit

---
+++ extime.py
@@ -0,0 +1,9 @@
+# Starting with the required imports
+from datetime import datetime, date, time, timedelta
+# Getting current date
+today = date.today()
+print ("Today's date is ", today)
+print ("Current year is ", today.year)
+print ("Current month is ", today.month)
+print ("Current day is ", today.day)
+print ("Current weekday is ", today.weekday())
